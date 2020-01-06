fcda41705cb0539c4685580627e798f84948e07d
SHANT GARABETIAN <39273575+shantgarabetian@users.noreply.github.com>
2020-01-06 14:30:56 -0800

    Add files via upload

---
+++ LondonTube/55/TICKETS.Designer.vb
@@ -0,0 +1,232 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.RadioButton3 = New System.Windows.Forms.RadioButton()
+        Me.RadioButton2 = New System.Windows.Forms.RadioButton()
+        Me.RadioButton1 = New System.Windows.Forms.RadioButton()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.RadioButton6 = New System.Windows.Forms.RadioButton()
+        Me.RadioButton5 = New System.Windows.Forms.RadioButton()
+        Me.RadioButton4 = New System.Windows.Forms.RadioButton()
+        Me.btncalculate = New System.Windows.Forms.Button()
+        Me.btnclear = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.lblprice = New System.Windows.Forms.Label()
+        Me.GroupBox1.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.RadioButton3)
+        Me.GroupBox1.Controls.Add(Me.RadioButton2)
+        Me.GroupBox1.Controls.Add(Me.RadioButton1)
+        Me.GroupBox1.Font = New System.Drawing.Font("Arial Rounded MT Bold", 10.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.GroupBox1.Location = New System.Drawing.Point(106, 131)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(271, 153)
+        Me.GroupBox1.TabIndex = 0
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "SELECT YOUR ZONE"
+        '
+        'RadioButton3
+        '
+        Me.RadioButton3.AutoSize = True
+        Me.RadioButton3.Location = New System.Drawing.Point(16, 119)
+        Me.RadioButton3.Name = "RadioButton3"
+        Me.RadioButton3.Size = New System.Drawing.Size(123, 24)
+        Me.RadioButton3.TabIndex = 2
+        Me.RadioButton3.TabStop = True
+        Me.RadioButton3.Text = "ZONES  1-6"
+        Me.RadioButton3.UseVisualStyleBackColor = True
+        '
+        'RadioButton2
+        '
+        Me.RadioButton2.AutoSize = True
+        Me.RadioButton2.Location = New System.Drawing.Point(16, 76)
+        Me.RadioButton2.Name = "RadioButton2"
+        Me.RadioButton2.Size = New System.Drawing.Size(123, 24)
+        Me.RadioButton2.TabIndex = 1
+        Me.RadioButton2.TabStop = True
+        Me.RadioButton2.Text = "ZONES  1-5"
+        Me.RadioButton2.UseVisualStyleBackColor = True
+        '
+        'RadioButton1
+        '
+        Me.RadioButton1.AutoSize = True
+        Me.RadioButton1.Location = New System.Drawing.Point(16, 31)
+        Me.RadioButton1.Name = "RadioButton1"
+        Me.RadioButton1.Size = New System.Drawing.Size(123, 24)
+        Me.RadioButton1.TabIndex = 0
+        Me.RadioButton1.TabStop = True
+        Me.RadioButton1.Text = "ZONES  1-3"
+        Me.RadioButton1.UseVisualStyleBackColor = True
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Controls.Add(Me.RadioButton6)
+        Me.GroupBox2.Controls.Add(Me.RadioButton5)
+        Me.GroupBox2.Controls.Add(Me.RadioButton4)
+        Me.GroupBox2.Font = New System.Drawing.Font("Arial Rounded MT Bold", 10.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.GroupBox2.Location = New System.Drawing.Point(408, 131)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(312, 153)
+        Me.GroupBox2.TabIndex = 1
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "SELECT YOUR CURRENCY"
+        '
+        'RadioButton6
+        '
+        Me.RadioButton6.AutoSize = True
+        Me.RadioButton6.Location = New System.Drawing.Point(23, 119)
+        Me.RadioButton6.Name = "RadioButton6"
+        Me.RadioButton6.Size = New System.Drawing.Size(90, 24)
+        Me.RadioButton6.TabIndex = 2
+        Me.RadioButton6.TabStop = True
+        Me.RadioButton6.Text = "EUROS"
+        Me.RadioButton6.UseVisualStyleBackColor = True
+        '
+        'RadioButton5
+        '
+        Me.RadioButton5.AutoSize = True
+        Me.RadioButton5.Location = New System.Drawing.Point(23, 72)
+        Me.RadioButton5.Name = "RadioButton5"
+        Me.RadioButton5.Size = New System.Drawing.Size(177, 24)
+        Me.RadioButton5.TabIndex = 1
+        Me.RadioButton5.TabStop = True
+        Me.RadioButton5.Text = "BRITISH POUNDS"
+        Me.RadioButton5.UseVisualStyleBackColor = True
+        '
+        'RadioButton4
+        '
+        Me.RadioButton4.AutoSize = True
+        Me.RadioButton4.Location = New System.Drawing.Point(23, 31)
+        Me.RadioButton4.Name = "RadioButton4"
+        Me.RadioButton4.Size = New System.Drawing.Size(139, 24)
+        Me.RadioButton4.TabIndex = 0
+        Me.RadioButton4.TabStop = True
+        Me.RadioButton4.Text = "US DOLLARS"
+        Me.RadioButton4.UseVisualStyleBackColor = True
+        '
+        'btncalculate
+        '
+        Me.btncalculate.BackColor = System.Drawing.Color.FromArgb(CType(CType(192, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.btncalculate.Font = New System.Drawing.Font("Arial Rounded MT Bold", 10.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btncalculate.Location = New System.Drawing.Point(144, 308)
+        Me.btncalculate.Name = "btncalculate"
+        Me.btncalculate.Size = New System.Drawing.Size(177, 43)
+        Me.btncalculate.TabIndex = 2
+        Me.btncalculate.Text = "CALCULATE"
+        Me.btncalculate.UseVisualStyleBackColor = False
+        '
+        'btnclear
+        '
+        Me.btnclear.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.btnclear.Font = New System.Drawing.Font("Arial Rounded MT Bold", 10.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnclear.Location = New System.Drawing.Point(443, 308)
+        Me.btnclear.Name = "btnclear"
+        Me.btnclear.Size = New System.Drawing.Size(177, 43)
+        Me.btnclear.TabIndex = 3
+        Me.btnclear.Text = "CLEAR"
+        Me.btnclear.UseVisualStyleBackColor = False
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Arial Rounded MT Bold", 13.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(241, 37)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(302, 28)
+        Me.Label1.TabIndex = 4
+        Me.Label1.Text = "LONDON TUBE TICKETS"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(486, 86)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(100, 22)
+        Me.TextBox1.TabIndex = 5
+        Me.TextBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Arial Rounded MT Bold", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(140, 85)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(297, 23)
+        Me.Label2.TabIndex = 6
+        Me.Label2.Text = "ENTER NUMBER OF TICKETS"
+        '
+        'lblprice
+        '
+        Me.lblprice.AutoSize = True
+        Me.lblprice.Font = New System.Drawing.Font("Arial Rounded MT Bold", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblprice.Location = New System.Drawing.Point(271, 392)
+        Me.lblprice.Name = "lblprice"
+        Me.lblprice.Size = New System.Drawing.Size(0, 23)
+        Me.lblprice.TabIndex = 7
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.lblprice)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.btnclear)
+        Me.Controls.Add(Me.btncalculate)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Name = "Form1"
+        Me.Text = "TUBE TICKETS"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.GroupBox2.ResumeLayout(False)
+        Me.GroupBox2.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents GroupBox2 As GroupBox
+    Friend WithEvents RadioButton3 As RadioButton
+    Friend WithEvents RadioButton2 As RadioButton
+    Friend WithEvents RadioButton1 As RadioButton
+    Friend WithEvents RadioButton6 As RadioButton
+    Friend WithEvents RadioButton5 As RadioButton
+    Friend WithEvents RadioButton4 As RadioButton
+    Friend WithEvents btncalculate As Button
+    Friend WithEvents btnclear As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents lblprice As Label
+End Class
---
+++ LondonTube/55/TICKETS.vb
@@ -0,0 +1,99 @@
+﻿Public Class Form1
+    Const firstZone As Decimal = 4.9
+    Const secondZone As Decimal = 5.9
+    Const thirdZone As Decimal = 6.0
+    Dim numOftickets As Integer
+    Dim cost As Decimal
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Clear()
+    End Sub
+
+    Private Sub Clear()
+        lblprice.Text = ""
+        TextBox1.Text = ""
+        RadioButton1.Checked = False
+        RadioButton2.Checked = False
+        RadioButton3.Checked = False
+        RadioButton4.Checked = False
+        RadioButton5.Checked = True
+        RadioButton6.Checked = False
+        TextBox1.Focus()
+    End Sub
+
+    Private Sub validationCheck()
+
+        If IsNumeric(TextBox1.Text) Then
+            Try
+                numOftickets = Convert.ToInt32(TextBox1.Text)
+            Catch Wronginput As Exception
+                MsgBox("Bad Number", MsgBoxStyle.OkOnly Or MsgBoxStyle.Exclamation, "ATTENTION")
+                Clear()
+                Exit Sub
+            End Try
+
+            If TextBox1.Text <= 0 Or TextBox1.Text > 99 Then
+                MsgBox("FormatNumber Of Tickets Must Be Over Zero And Less than 100", MsgBoxStyle.OkOnly Or MsgBoxStyle.Exclamation, "ATTENTION")
+
+            End If
+            If RadioButton1.Checked = False And RadioButton2.Checked = False And RadioButton3.Checked = False Then
+                MsgBox("Check Zones Please", MsgBoxStyle.OkOnly Or MsgBoxStyle.Exclamation, "ATTENTION")
+                Clear()
+            End If
+
+        ElseIf TextBox1.Text = "" Then
+            MsgBox("Enter Number Of Tickets Please", MsgBoxStyle.OkOnly Or MsgBoxStyle.Exclamation, "ATTENTION")
+            Clear()
+        Else
+            MsgBox("Number Of Tickets Must be Numeric", MsgBoxStyle.OkOnly Or MsgBoxStyle.Exclamation, "ATTENTION")
+            Clear()
+            Exit Sub
+        End If
+    End Sub
+
+    Private Sub btncalculate_Click(sender As Object, e As EventArgs) Handles btncalculate.Click
+        validationCheck()
+        If numOftickets > 0 And numOftickets < 100 Then
+            If RadioButton1.Checked And RadioButton4.Checked Then
+                cost = numOftickets * firstZone
+                lblprice.Text = " The Cost of Zone 1-3 : " & cost.ToString("C")
+            End If
+            If RadioButton1.Checked And RadioButton5.Checked Then
+                cost = numOftickets * firstZone
+                lblprice.Text = " The Cost of Zone 1-3 : " & cost.ToString("C", System.Globalization.CultureInfo.CreateSpecificCulture("en-GB"))
+            End If
+            If RadioButton1.Checked And RadioButton6.Checked Then
+                cost = numOftickets * firstZone
+                lblprice.Text = " The Cost of Zone 1-3 : " & cost.ToString("C", System.Globalization.CultureInfo.CreateSpecificCulture("de-DE"))
+            End If
+            If RadioButton2.Checked And RadioButton4.Checked Then
+                cost = numOftickets * secondZone
+                lblprice.Text = " The Cost of Zone 1-5 : " & cost.ToString("C")
+            End If
+            If RadioButton2.Checked And RadioButton5.Checked Then
+                cost = numOftickets * secondZone
+                lblprice.Text = " The Cost of Zone 1-5 : " & cost.ToString("C", System.Globalization.CultureInfo.CreateSpecificCulture("en-GB"))
+            End If
+            If RadioButton2.Checked And RadioButton6.Checked Then
+                cost = numOftickets * secondZone
+                lblprice.Text = " The Cost of Zone 1-5 : " & cost.ToString("C", System.Globalization.CultureInfo.CreateSpecificCulture("de-DE"))
+            End If
+            If RadioButton3.Checked And RadioButton4.Checked Then
+                cost = numOftickets * thirdZone
+                lblprice.Text = " The Cost of Zone 1-6 : " & cost.ToString("C")
+            End If
+            If RadioButton3.Checked And RadioButton5.Checked Then
+                cost = numOftickets * thirdZone
+                lblprice.Text = " The Cost of Zone 1-6 : " & cost.ToString("C", System.Globalization.CultureInfo.CreateSpecificCulture("en-GB"))
+            End If
+            If RadioButton3.Checked And RadioButton6.Checked Then
+                cost = numOftickets * thirdZone
+                lblprice.Text = " The Cost of Zone 1-6 : " & cost.ToString("C", System.Globalization.CultureInfo.CreateSpecificCulture("de-DE"))
+            End If
+        End If
+    End Sub
+
+    Private Sub btnclear_Click(sender As Object, e As EventArgs) Handles btnclear.Click
+        Clear()
+    End Sub
+End Class
